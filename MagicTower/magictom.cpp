214a45f175eb86ea09152c42f9ca9c61e31cedc9
JimmyZJX <604590822@qq.com>
2013-12-16 15:45:51 +0800

    small update. sound effect for step.

--- MagicTower/magictom.cpp
+++ MagicTower/magictom.cpp
@@ -6,6 +6,7 @@ MagicTom::MagicTom()
 {
     pix = new QPixmap(":/images/Tom");
     x = y = 0;
+    mSound = new QSound(":/sounds/step");
 }
 
 void MagicTom::paint(QPainter *painter)
@@ -21,6 +22,7 @@ void MagicTom::moveTo(int tx, int ty)
 void MagicTom::setStep(int dx, int dy)
 {
     this->dx = dx, this->dy = dy;
+    mSound->play();
 }
 
 void MagicTom::move()
--- MagicTower/magictom.h
+++ MagicTower/magictom.h
@@ -3,11 +3,15 @@
 
 #include "magicdisplayobject.h"
 
+#include <QSound>
+
 class MagicTom : public MagicDisplayObject
 {
     QPixmap *pix;
     int x, y;
     int dx, dy;
+
+    QSound *mSound;
 
 public:
     MagicTom();
