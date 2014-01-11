a97b979244c840132603c3f16eada6b41c5efd64
KevinXuxuxu <Kevin.xu.fangzhou@gmail.com>
2014-01-11 12:04:50 +0800

    exp and money system finished

--- MagicTower/status.cpp
+++ MagicTower/status.cpp
@@ -25,6 +25,8 @@ void Status::paintEvent(QPaintEvent *event)
     painter.drawText(0, 90, QString("Attack: ") + QString::number((*mMap->Tom())["attack"].getInt()));
     painter.drawText(0, 120, QString("Defend: ") + QString::number((*mMap->Tom())["defend"].getInt()));
     painter.drawText(0, 150, QString("Level: ") + QString::number((*mMap->Tom())["level"].getInt()));
+    painter.drawText(0, 180, QString("Exp: ") + QString::number((*mMap->Tom())["exp"].getInt()));
+    painter.drawText(0, 210, QString("Money: ") + QString::number((*mMap->Tom())["money"].getInt()));
 
     painter.end();
 }
